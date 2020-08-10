f1d466c09fb444bd4de845d2d20d472b226cbbbd
tanmoy43 <43274420+tanmoy43@users.noreply.github.com>
2020-08-10 23:37:19 +0600

    Fontend Setup Complete

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+// Index Page
+Route::get('/', 'index@indexpageview')->name('index');
+// Shop Page
+Route::get('/Shop', 'ShopController@shoppageview')->name('shop');
+// Shop Details
+Route::get('/Shop-Details', function () {
+    return view('pages.shop_details');
+})->name('shop_details');
+// Shopping Cart
+Route::get('/Shopping-Cart', function () {
+    return view('pages.shopping_cart');
+})->name('shopping_cart');
+// Checkout
+Route::get('/Checkout', function () {
+    return view('pages.checkout');
+})->name('checkout');
+// Blog Details
+Route::get('/Blog-Details', function () {
+    return view('pages.blog_details');
+})->name('blog_details');
+// Blog
+Route::get('/Blog', function () {
+    return view('pages.blog');
+})->name('blog');
+//Contact
+Route::get('/Contact', function () {
+    return view('pages.contact');
+})->name('contact');
